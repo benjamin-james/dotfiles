90d09f833f320bdb93e19dfdd90984ff6d1e141d
Ben James <benjames336@gmail.com>
2015-05-14 10:01:31 -0500

    Added formatting functions

--- init.el
+++ init.el
@@ -35,7 +35,8 @@
 
 ;;custom themes (wilson and hickey are both great)
 (add-to-list 'custom-theme-load-path
-             (concat emacs-root "themes"))
+	     (concat emacs-root "themes"))
+(load-theme 'wilson t)
 
 ;;multi-term and my key binding for it
 (add-to-list 'load-path
@@ -69,6 +70,56 @@
  ;; optional keyboard short-cut
  (global-set-key "\C-xm" 'browse-url-at-point)
 
+;;
+;; spacing and formatting
+;;
+(setq nuke-trailing-whitespace-p t)
+(defun nuke-trailing-whitespace ()
+  "Nuke all trailing whitespace in the buffer.
+Whitespace in this case is just spaces or tabs.
+This is a useful function to put on write-file-hooks.
+
+If the variable `nuke-trailing-whitespace-p` is `nil`, this function is
+disabled.  If `t`, unreservedly strip trailing whitespace.
+If not `nil` and not `t`, query for each instance."
+  (interactive)
+  (and nuke-trailing-whitespace-p
+       (save-match-data
+	 (save-excursion
+	   (save-restriction
+	     (widen)
+	     (goto-char (point-min))
+	     (cond ((eq nuke-trailing-whitespace-p t)
+		    (while (re-search-forward "[        ]+$" (point-max) t)
+		      (delete-region (match-beginning 0) (match-end 0))))
+		   (t
+		    (query-replace-regexp "[    ]+$" "")))))))
+  ;; always return nil, in case this is on write-file-hooks.
+  nil)
+(defun how-many-region (begin end regexp &optional interactive)
+  "Print number of non-trivial matches for REGEXP in region.
+Non-interactive arguments are Begin End Regexp"
+  (interactive "r\nsHow many matches for (regexp): \np")
+  (let ((count 0) opoint)
+    (save-excursion
+      (setq end (or end (point-max)))
+      (goto-char (or begin (point)))
+      (while (and (< (setq opoint (point)) end)
+		  (re-search-forward regexp end t))
+	(if (= opoint (point))
+	    (forward-char 1)
+	  (setq count (1+ count))))
+      (if interactive (message "%d occurrences" count))
+      count)))
+
+(defun infer-indentation-style ()
+  ;; if our source file uses tabs, we use tabs, if spaces spaces, and if
+  ;; neither, we use the current indent-tabs-mode
+  (let ((space-count (how-many-region (point-min) (point-max) "^  "))
+	(tab-count (how-many-region (point-min) (point-max) "^\t")))
+    (if (> space-count tab-count) (setq indent-tabs-mode nil))
+    (if (> tab-count space-count) (setq indent-tabs-mode t))))
+
 ;;c indent style
 (defun c-lineup-arglist-tabs-only (ignored)
   "Line up argument lists by tabs, not spaces"
@@ -98,6 +149,7 @@
                                        filename))
                 (setq indent-tabs-mode t)
                 (setq show-trailing-whitespace t)
+		(setq nuke-trailing-whitespace t)
                 (c-set-style "linux-tabs-only")))))
 
 
