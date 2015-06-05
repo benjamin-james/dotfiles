251c568d20a09a8e9cff8e92f992ac144f73de65
Benjamin James <benjamin-james@users.noreply.github.com>
2015-06-05 14:53:04 -0500

    Automatic ELPA downloads and organization
    Former-commit-id: 0d6525c952bf782992963da71fe32b5ee912ddfe

--- init.el
+++ init.el
@@ -1,53 +1,62 @@
-;;;
-;;; Ben's .emacs file
-;;;
+(require 'package)
+(add-to-list 'package-archives
+	     '("melpa" . "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives
+	     '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives
+	     '("org" . "http://orgmode.org/elpa/") t)
+(package-initialize)
 
-;;use common lisp stuff too
-(require 'cl)
+(defconst my-packages
+  '(c-eldoc
+    auto-complete
+    monokai-theme
+    nyan-mode
+    slime
+    multi-term
+    yasnippet))
 
-;;define the emacs directory (cross platform)
+(defun install-packages ()
+  "Install the packages"
+  (interactive)
+  (unless package-archive-contents
+    (package-refresh-contents))
+  (dolist (package my-packages)
+    (unless (package-installed-p package)
+      (package-install package))))
+(install-packages)
+
 (defvar emacs-root
   (eval-when-compile
     (if (or (eq system-type 'cygwin)
 	    (eq system-type 'gnu/linux)
 	    (eq system-type 'linux)
 	    (eq system-type 'darwin))
-	"~/.emacs.d/" "z:/.emacs.d/"))
+	"~/\\.emacs\\.d/" "%HOMEPATH%/emacs\\.d/"))
   "Path to the .emacs.d directory")
 
-;;startup without the regular splashscreen and message
-(setq inhibit-splash-screen t)
-(setq inhibit-startup-message t)
+(add-to-list 'load-path
+	     (concat emacs-root "custom"))
+(require 'cl)
+(require 'cl-lib)
+(require 'whitespace)
 
-;;eldoc enabled - displays function headers
-(add-hook 'c-mode-hook 'c-turn-on-eldoc-mode)
-
-;;package archive list
-(require 'package)
-(add-to-list 'package-archives
-	     '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives
-	     '("melpa" . "http://melpa.milkbox.net/packages/"))
-(add-to-list 'package-archives
-	     '("org" . "http://orgmode.org/elpa/") t)
-
-(package-initialize)
-(setq url-http-attempt-keepalives nil)
-
-;;slime - make sure to change /usr/bin/sbcl if sbcl is not there for you
-(setq inferior-lisp-program "/usr/bin/clisp")
-(setq slime-contribs '(slime-fancy))
-
-;;ido
 (require 'ido)
 (ido-mode t)
+(require 'nyan-mode)
+(nyan-mode)
+(require 'yasnippet)
+(yas-global-mode 1)
+(require 'slime)
+(setq inferior-lisp-program "/usr/bin/sbcl")
+(setq slime-contribs '(slime-fancy))
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories
+	     (concat emacs-root "ac-dict")
+(ac-config-default)
+(ac-set-trigger-key "TAB")
+(ac-set-trigger-key "<tab>")
 
-;;custom themes (wilson and hickey are both great)
-(add-to-list 'custom-theme-load-path
-	     (concat emacs-root "themes"))
-(load-theme 'monokai t)
-
-;;multi-term and my key binding for it
 (require 'multi-term)
 (setq multi-term-program "/bin/bash")
 (global-set-key (kbd "M-t") 'multi-term)
@@ -58,99 +67,33 @@
 (global-set-key (kbd "C-x M-s") 'multi-term-dedicated-select)
 (global-set-key (kbd "C-x M-c") 'multi-term-dedicated-close)
 
-;;yasnippet and autocomplete
-(require 'yasnippet)
-(yas-global-mode 1)
-(require 'auto-complete-config)
-(add-to-list 'ac-dictionary-directories
-	     (concat emacs-root "ac-dict"))
-(ac-config-default)
-;;trigger key
-(ac-set-trigger-key "TAB")
-(ac-set-trigger-key "<tab>")
-(nyan-mode)
 
-;;
-;; spacing and formatting
-;;
-(setq nuke-trailing-whitespace-p t)
-(defun nuke-trailing-whitespace ()
-  "Nuke all trailing whitespace in the buffer.
-Whitespace in this case is just spaces or tabs.
-This is a useful function to put on write-file-hooks.
 
-If the variable `nuke-trailing-whitespace-p` is `nil`, this function is
-disabled.  If `t`, unreservedly strip trailing whitespace.
-If not `nil` and not `t`, query for each instance."
-  (interactive)
-  (and nuke-trailing-whitespace-p
-       (save-match-data
-	 (save-excursion
-	   (save-restriction
-	     (widen)
-	     (goto-char (point-min))
-	     (cond ((eq nuke-trailing-whitespace-p t)
-		    (while (re-search-forward "[        ]+$" (point-max) t)
-		      (delete-region (match-beginning 0) (match-end 0))))
-		   (t
-		    (query-replace-regexp "[    ]+$" "")))))))
-  ;; always return nil, in case this is on write-file-hooks.
-  nil)
-(defun how-many-region (begin end regexp &optional interactive)
-  "Print number of non-trivial matches for REGEXP in region.
-Non-interactive arguments are Begin End Regexp"
-  (interactive "r\nsHow many matches for (regexp): \np")
-  (let ((count 0) opoint)
-    (save-excursion
-      (setq end (or end (point-max)))
-      (goto-char (or begin (point)))
-      (while (and (< (setq opoint (point)) end)
-		  (re-search-forward regexp end t))
-	(if (= opoint (point))
-	    (forward-char 1)
-	  (setq count (1+ count))))
-      (if interactive (message "%d occurrences" count))
-      count)))
+(setq inhibit-splash-screen t)
+(setq inhibit-startup-message t)
 
-(defun infer-indentation-style ()
-  ;; if our source file uses tabs, we use tabs, if spaces spaces, and if
-  ;; neither, we use the current indent-tabs-mode
-  (let ((space-count (how-many-region (point-min) (point-max) "^  "))
-	(tab-count (how-many-region (point-min) (point-max) "^\t")))
-    (if (> space-count tab-count) (setq indent-tabs-mode nil))
-    (if (> tab-count space-count) (setq indent-tabs-mode t))))
+;;formatting
+(setq-default indent-tabs-mode t)
+(setq-default tab-width 8)
 
-;;c indent style
-(defun c-lineup-arglist-tabs-only (ignored)
-  "Line up argument lists by tabs, not spaces"
-  (let* ((anchor (c-langelem-pos c-syntactic-element))
-	  (column (c-langelem-2nd-pos c-syntactic-element))
-	   (offset (- (1+ column) anchor))
-	    (steps (floor offset c-basic-offset)))
-    (* (max steps 1)
-       c-basic-offset)))
-
-(add-hook 'c-mode-common-hook
-          (lambda ()
-	    (setq indent-tabs-mode t)
+(add-hook 'prog-mode-hook
+	  (lambda ()
+	    (interactive)
 	    (setq show-trailing-whitespace t)
-	    (setq nuke-trailing-whitespace t)
-            ;; Add my style
-            (c-add-style
-             "ben-style"
-             '("linux" (c-offsets-alist
-                        (arglist-cont-nonempty
-                         c-lineup-gcc-asm-reg
-                         c-lineup-arglist-tabs-only))))))
+	    (setq nuke-trailing-whitespace t)))
+;;my theme
+(load-theme 'monokai t)
 
-(add-hook 'c-mode-hook
-          (lambda ()
-	    (add-to-list 'ac-sources 'ac-source-c-headers)
-	    (add-to-list 'ac-sources 'ac-source-c-header-symbols t)
-	    (setq indent-tabs-mode t)
-	    (setq show-trailing-whitespace t)
-	    (setq nuke-trailing-whitespace t)
-	    (c-set-style "ben-style")))
+(c-add-style "ben-style"
+	     '("linux" (c-offsets-alist
+			(arglist-cont-nonempty
+			 c-lineup-gcc-asm-reg
+			 c-lineup-arglist-tabs-only))))
+(setq c-default-style "ben-style")
+
+;;displays function header in minibuffer
+(add-hook 'c-mode-hook 'c-turn-on-eldoc-mode)
+
 ;;
 ;; Variables for emacs in X
 ;;
