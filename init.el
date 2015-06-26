6d6a64d405668a7a2ff29a8341e917606eb58e6e
Ben James <benjamin-james@utulsa.edu>
2015-06-25 20:27:50 -0500

    ecb mode
    Former-commit-id: 7464f800c6ff97499391e7ad6305f9948cf11b3d

--- init.el
+++ init.el
@@ -20,6 +20,7 @@
     company
     company-irony
     cl-generic
+    ecb
     flycheck
     flycheck-irony
     golden-ratio
@@ -71,6 +72,7 @@
 
 (require 'ac-c-headers)
 (require 'chess)
+(require 'ecb)
 (require 'flycheck)
 (add-hook 'after-init-hook #'global-flycheck-mode)
 (require 'flycheck-irony)
@@ -230,6 +232,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ecb-compile-window-height 0.25)
+ '(ecb-compile-window-temporally-enlarge (quote after-selection))
+ '(ecb-compile-window-width (quote edit-window))
+ '(ecb-layout-name "ben-layout")
+
  '(font-use-system-font t)
  '(fringe-mode 0 nil (fringe))
  '(menu-bar-mode nil)
