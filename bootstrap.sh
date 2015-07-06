65b8037db2736e12168e7c5fc0352e15a28f5a15
Benjamin James <benjamin-james@utulsa.edu>
2015-07-06 15:51:56 -0500

    yay
    Former-commit-id: 03033cd2cb632a0a80e174736c1b36ad4e226743

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 #Must have sbcl or clisp and llvm, clang, and cmake installed
 #It wouldn't hurt to have most and ack installed too.
-cp .bashrc ~
-cp .emacs ~
+cp bashrc ~/.bashrc
+cp emacs ~/.emacs
 
+if [[ $(which systemctl 2>/dev/null) ]]; then
+    mkdir -p ~/.config/systemd/user/ && cp emacsd.service ~/.config/systemd/user/
+fi
+mkdir -p ~/.config/git && cp gitignore ~/.config/git/ignore
+cp gitconfig ~/.gitconfig
 #This installs all ELPA stuff and also installs irony server
 emacs -nw --eval '(irony-server-install)'
