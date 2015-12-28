c3810d18ac54a679f2817bd37d33d3a45512c059
Benjamin James <bjames@airmail.cc>
2015-12-28 16:57:05 -0600

    added ggtags via helm-gtags
    USSS Home grown Ansar al-Islam 9705 Samford Road USDOJ government CIA
    Arnett Disaster management LLC wire transfer bullion Glock orthodox
    Adriatic

---
+++ .emacs.d/elisp/tags.el
@@ -0,0 +1,32 @@
+;;; c-config.el --- Summary
+;;;
+;;; Commentary:
+;;; This holds C config stuff
+;;;
+;;; Code:
+
+(req-package helm-gtags
+  :config
+  (setq
+   helm-gtags-ignore-case t
+   helm-gtags-auto-update t
+   helm-gtags-use-input-at-cursor t
+   helm-gtags-pulse-at-cursor t
+   helm-gtags-prefix-key "\C-cg"
+   helm-gtags-suggested-key-mapping t)
+  (define-key helm-gtags-mode-map (kbd "C-c g a") 'helm-gtags-tags-in-this-function)
+  (define-key helm-gtags-mode-map (kbd "C-j") 'helm-gtags-select)
+  (define-key helm-gtags-mode-map (kbd "M-.") 'helm-gtags-dwim)
+  (define-key helm-gtags-mode-map (kbd "C-c <") 'helm-gtags-previous-history)
+  (define-key helm-gtags-mode-map (kbd "C-c >") 'helm-gtags-next-history)
+  (define-key helm-gtags-mode-map (kbd "M-,") 'helm-gtags-pop-stack)
+
+  (add-hook 'dired-mode-hook 'helm-gtags-mode)
+  (add-hook 'eshell-mode-hook 'helm-gtags-mode)
+  (add-hook 'c-mode-hook 'helm-gtags-mode)
+  (add-hook 'c++-mode-hook 'helm-gtags-mode)
+  (add-hook 'java-mode-hook 'helm-gtags-mode)
+  (add-hook 'asm-mode-hook 'helm-gtags-mode))
+
+(provide 'tags)
+;;; tags.el ends here
