38bd0b1d43edc44ea3bbd0502b572e6641929092
Benjamin James <bjames@airmail.cc>
2016-01-02 22:32:33 -0600

    made gnus pretty and stuff
    XS4ALL Mafia Shayet-13 Water borne Security Consulting PFS DREC MARTA
    Legion of Doom Becker White Powder Taliban Hollyhock electronic
    surveillance Red Cross

--- .emacs.d/elisp/mail-config.el
+++ .emacs.d/elisp/mail-config.el
@@ -6,11 +6,11 @@
 ;;; Code:
 
 (setq
- gnus-select-method '(nnmaildir "inbox" (directory "~/.mail/"))
+ gnus-select-method '(nnmaildir "default" (directory "~/.mail/"))
  mail-sources '((maildir :path "~/.mail/" :subdirs ("cur" "new")))
  mail-source-delete-incoming t)
 (setq gnus-secondary-select-methods nil)
-(setq gnus-message-archive-group "nnmaildir+inbox:outbox")
+(setq gnus-message-archive-group "nnmaildir+default:outbox")
 
 ;;mutt settings
 (setq auto-mode-alist (append '(("/tmp/mutt.*" . mail-mode)) auto-mode-alist))
@@ -20,6 +20,30 @@
   (auto-fill-mode 1)
   (abbrev-mode 1)
   (local-set-key "\C-Xk" 'server-edit))
+
+;; http://groups.google.com/group/gnu.emacs.gnus/browse_thread/thread/a673a74356e7141f
+(when window-system
+  (setq gnus-sum-thread-tree-indent "  ")
+  (setq gnus-sum-thread-tree-root "") ;; "● ")
+  (setq gnus-sum-thread-tree-false-root "") ;; "◯ ")
+  (setq gnus-sum-thread-tree-single-indent "") ;; "◎ ")
+  (setq gnus-sum-thread-tree-vertical        "│")
+  (setq gnus-sum-thread-tree-leaf-with-other "├─► ")
+  (setq gnus-sum-thread-tree-single-leaf     "╰─► "))
+
+(setq gnus-summary-line-format
+      (concat
+       "%0{%U%R%z%}"
+       "%3{│%}" "%1{%d%}" "%3{│%}" ;; date
+       "  "
+       "%4{%-20,20f%}"               ;; name
+       "  "
+       "%3{│%}"
+       " "
+       "%1{%B%}"
+       "%s\n"))
+
+(setq gnus-summary-display-arrow t)
 
 (add-hook 'mail-mode-hook 'my-mail-mode-hook)
 
