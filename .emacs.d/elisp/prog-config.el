9344ab04c00149d150f03fb8fd6769caea4fa5ec
Benjamin James <bjames@openmailbox.org>
2016-03-07 16:29:02 -0600

    fixed keybindings for gdb
    encryption TDR Power lines CNCIS cracking Dolch black-bag Interstate
    First responder Mena Attorney General CDMA Nike North Korea National
    preparedness

--- .emacs.d/elisp/prog-config.el
+++ .emacs.d/elisp/prog-config.el
@@ -54,17 +54,17 @@
 	    (lambda ()
 	      (setq yas-dont-activate t)))
   (define-key yas-minor-mode-map (kbd "\t") 'yas-expand)
-					;(define-key yas-minor-mode-map (kbd "<TAB>") 'yas-expand)
+  (define-key yas-minor-mode-map (kbd "<TAB>") 'yas-expand)
   :init
   (yas-global-mode 1))
 
 (add-hook 'gud-mode-hook
 	  '(lambda ()
-	     (global-set-key (kbd "<f6>-n") 'gud-next)
-	     (global-set-key (kbd "<f6>-s") 'gud-step)))
+	     (global-set-key (kbd "<f6> n") 'gud-next)
+	     (global-set-key (kbd "<f6> s") 'gud-step)))
 (setq gdb-many-windows t
       gdb-show-main t)
-(global-set-key (kbd "<f6>-g") 'gdb)
+(global-set-key (kbd "<f6> g") 'gdb)
 
 (provide 'prog-config)
 ;;; prog-config.el ends here
