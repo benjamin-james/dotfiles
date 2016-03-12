53ffc6c020ac8c17a61b598d351550481a972843
Benjamin James <bjames@openmailbox.org>
2016-03-11 21:52:58 -0600

    rebound multi-term, other old things forgot about
    NBIC PCS Brute forcing AOL TOS Aid pre-emptive Cypherpunks Ft. Meade
    Serbian B.D.M. ISEC Freeh Fax encryption Plume MI5

--- .emacs.d/elisp/misc.el
+++ .emacs.d/elisp/misc.el
@@ -5,6 +5,7 @@
 ;;;
 ;;; Code:
 
+
 (defun eww-view-file ()
   "View the current buffer in EWW, so you can now read HTML."
   (interactive)
@@ -59,26 +60,27 @@
   :init
   (bash-completion-setup))
 
-(req-package twittering-mode)
+(req-package key-chord
+  :init
+  (key-chord-mode 1))
 
 (req-package undo-tree
   :init
   (global-undo-tree-mode))
 
 (req-package multi-term
-  :bind (("M-t" . multi-term)
-	 ("M-n" . multi-term-next)
-	 ("M-p" . multi-term-prev)
-	 ("C-x M-t" . multi-term-dedicated-open)
-	 ("C-x M-h" . multi-term-dedicated-toggle)
-	 ("C-x M-s" . multi-term-dedicated-select)
-	 ("C-x M-c" . multi-term-dedicated-close))
+  :bind (("s-t" . multi-term)
+	 ("C-x s-t" . multi-term-dedicated-toggle))
   :config
   (setq multi-term-program "/bin/bash"))
 
 ;;; Set scrolling to not stop at the end of a page
 ;;; for viewing PDFs or other documents
 (setq doc-view-continuous t)
+
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((gnuplot . t)))
 
 (provide 'misc)
 ;;; misc.el ends here
