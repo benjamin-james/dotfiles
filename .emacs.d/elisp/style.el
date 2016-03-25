b6e7ac8b9b5a3d265022208f81eff2580af93f9b
Benjamin James <bjames@openmailbox.org>
2016-03-25 00:44:48 -0500

    added dtrt-indent
    State of emergency CIA Flashbangs lock picking Suicide bomber
    press-release Blizzard Cap-Stun Oratory DITSA UOP Hazmat import Forte
    M.P.R.I.

--- .emacs.d/elisp/style.el
+++ .emacs.d/elisp/style.el
@@ -5,9 +5,13 @@
 ;;;
 ;;; Code:
 
-(setq-default indent-tabs-mode t)
-(setq-default tab-width 8)
-(setq c-default-style "ben-style")
+(setq-default indent-tabs-mode t
+              tab-width 8
+              c-default-style "ben-style")
+
+(req-package dtrt-indent
+  :config
+  (dtrt-indent-mode 1))
 
 (provide 'style)
 ;;; style.el ends here
