6ed602907a8384a6094da8dbe5a244655508e78b
Benjamin James <bjames@openmailbox.org>
2016-04-28 15:41:23 -0500

    Added alot more hydras
    asset Mace UTU MSCJ Bomb squad jihad Planet-1 Tamil Tigers WINGS
    Shelter-in-place S/Key Sears Tower InfoSec Domestic security M72750

--- .emacs.d/elisp/my-hydras.el
+++ .emacs.d/elisp/my-hydras.el
@@ -66,11 +66,298 @@ _SPC_ cancel    _o_nly this     _d_elete
           (setq this-command 'winner-undo)))
    ("Z" winner-redo)
    ("SPC" nil))
+
 (global-set-key (kbd "s-w") 'hydra-window/body)
+(global-set-key (kbd "H-w") 'hydra-window/body)
 
 (defhydra hydra-zoom (global-map "<f2>")
   "zoom"
   ("g" text-scale-increase "in")
-  ("l" text-scale-decrease "out")))
+  ("l" text-scale-decrease "out"))
+
+(defhydra hydra-yank-pop ()
+  "yank"
+  ("C-y" yank nil)
+  ("M-y" yank-pop nil)
+  ("y" (yank-pop 1) "next")
+  ("Y" (yank-pop -1) "prev")
+  ("l" helm-show-kill-ring "list" :color blue))
+(global-set-key (kbd "M-y") #'hydra-yank-pop/yank-pop)
+(global-set-key (kbd "C-y") #'hydra-yank-pop/yank)
+
+
+(global-set-key
+ (kbd "C-n")
+ (defhydra hydra-move
+   (:body-pre (next-line))
+   "move"
+   ("n" next-line)
+   ("p" previous-line)
+   ("f" forward-char)
+   ("b" backward-char)
+   ("a" beginning-of-line)
+   ("e" move-end-of-line)
+   ("v" scroll-up-command)
+   ("V" scroll-down-command)
+   ("l" recenter-top-bottom)
+   ("<SPC>" nil :color blue)))
+
+(defhydra hydra-page (ctl-x-map "" :pre (widen))
+  "page"
+  ("]" forward-page "next")
+  ("[" backward-page "prev")
+  ("n" narrow-to-page "narrow" :bind nil :exit t))
+
+(defhydra hydra-goto-line (goto-map "")
+  "goto-line"
+  ("g" goto-line "go")
+  ("m" set-mark-command "mark" :bind nil)
+  ("q" nil "quit"))
+
+(global-set-key (kbd "H-C-t")
+(defhydra hydra-transpose (:color red)
+  "Transpose"
+  ("c" transpose-chars "characters")
+  ("w" transpose-words "words")
+  ("o" org-transpose-words "Org mode words")
+  ("l" transpose-lines "lines")
+  ("s" transpose-sentences "sentences")
+  ("e" org-transpose-elements "Org mode elements")
+  ("p" transpose-paragraphs "paragraphs")
+  ("t" org-transpose-table-at-point "Org mode table")
+  ("q" nil "cancel" :color blue)))
+
+(define-key Info-mode-map (kbd "?") #'hydra-info/body)
+(defhydra hydra-info (:color blue
+                      :hint nil)
+      "
+Info-mode:
+
+  ^^_]_ forward  (next logical node)       ^^_l_ast (←)        _u_p (↑)                             _f_ollow reference       _T_OC
+  ^^_[_ backward (prev logical node)       ^^_r_eturn (→)      _m_enu (↓) (C-u for new window)      _i_ndex                  _d_irectory
+  ^^_n_ext (same level only)               ^^_H_istory         _g_oto (C-u for new window)          _,_ next index item      _c_opy node name
+  ^^_p_rev (same level only)               _<_/_t_op           _b_eginning of buffer                virtual _I_ndex          _C_lone buffer
+  regex _s_earch (_S_ case sensitive)      ^^_>_ final         _e_nd of buffer                      ^^                       _a_propos
+
+  _1_ .. _9_ Pick first .. ninth item in the node's menu.
+
+"
+      ("]"   Info-forward-node)
+      ("["   Info-backward-node)
+      ("n"   Info-next)
+      ("p"   Info-prev)
+      ("s"   Info-search)
+      ("S"   Info-search-case-sensitively)
+
+      ("l"   Info-history-back)
+      ("r"   Info-history-forward)
+      ("H"   Info-history)
+      ("t"   Info-top-node)
+      ("<"   Info-top-node)
+      (">"   Info-final-node)
+
+      ("u"   Info-up)
+      ("^"   Info-up)
+      ("m"   Info-menu)
+      ("g"   Info-goto-node)
+      ("b"   beginning-of-buffer)
+      ("e"   end-of-buffer)
+
+      ("f"   Info-follow-reference)
+      ("i"   Info-index)
+      (","   Info-index-next)
+      ("I"   Info-virtual-index)
+
+      ("T"   Info-toc)
+      ("d"   Info-directory)
+      ("c"   Info-copy-current-node-name)
+      ("C"   clone-buffer)
+      ("a"   info-apropos)
+
+      ("1"   Info-nth-menu-item)
+      ("2"   Info-nth-menu-item)
+      ("3"   Info-nth-menu-item)
+      ("4"   Info-nth-menu-item)
+      ("5"   Info-nth-menu-item)
+      ("6"   Info-nth-menu-item)
+      ("7"   Info-nth-menu-item)
+      ("8"   Info-nth-menu-item)
+      ("9"   Info-nth-menu-item)
+
+      ("?"   Info-summary "Info summary")
+      ("h"   Info-help "Info help")
+      ("q"   Info-exit "Info exit")
+      ("C-g" nil "cancel" :color blue))
+
+(defhydra hydra-next-error
+    (global-map "C-x")
+    "
+Compilation errors:
+_j_: next error        _h_: first error    _q_uit
+_k_: previous error    _l_: last error
+"
+    ("`" next-error     nil)
+    ("j" next-error     nil :bind nil)
+    ("k" previous-error nil :bind nil)
+    ("h" first-error    nil :bind nil)
+    ("l" (condition-case err
+             (while t
+               (next-error))
+           (user-error nil))
+     nil :bind nil)
+    ("q" nil            nil :color blue))
+
+(global-set-key (kbd "H-p")
+(defhydra hydra-project (:color blue :hint nil :idle 0.4)
+        "
+                                                                    ╭────────────┐
+    Files             Search          Buffer             Do         │ Projectile │
+  ╭─────────────────────────────────────────────────────────────────┴────────────╯
+    [_f_] file          [_a_] ag          [_b_] switch         [_g_] magit
+    [_l_] file dwim     [_A_] grep        [_v_] show all       [_p_] commander
+    [_r_] recent file   [_s_] occur       [_V_] ibuffer        [_i_] info
+    [_d_] dir           [_S_] replace     [_K_] kill all
+    [_o_] other         [_t_] find tag
+    [_u_] test file     [_T_] make tags
+    [_h_] root
+                                                                        ╭────────┐
+    Other Window      Run             Cache              Do             │ Fixmee │
+  ╭──────────────────────────────────────────────────╯ ╭────────────────┴────────╯
+    [_F_] file          [_U_] test        [_kc_] clear         [_x_] TODO & FIXME
+    [_L_] dwim          [_m_] compile     [_kk_] add current   [_X_] toggle
+    [_D_] dir           [_c_] shell       [_ks_] cleanup
+    [_O_] other         [_C_] command     [_kd_] remove
+    [_B_] buffer
+  --------------------------------------------------------------------------------
+        "
+        ("<tab>" hydra-master/body "back")
+        ("<ESC>" nil "quit")
+        ("a"   projectile-ag)
+        ("A"   projectile-grep)
+        ("b"   projectile-switch-to-buffer)
+        ("B"   projectile-switch-to-buffer-other-window)
+        ("c"   projectile-run-async-shell-command-in-root)
+        ("C"   projectile-run-command-in-root)
+        ("d"   projectile-find-dir)
+        ("D"   projectile-find-dir-other-window)
+        ("f"   projectile-find-file)
+        ("F"   projectile-find-file-other-window)
+        ("g"   projectile-vc)
+        ("h"   projectile-dired)
+        ("i"   projectile-project-info)
+        ("kc"  projectile-invalidate-cache)
+        ("kd"  projectile-remove-known-project)
+        ("kk"  projectile-cache-current-file)
+        ("K"   projectile-kill-buffers)
+        ("ks"  projectile-cleanup-known-projects)
+        ("l"   projectile-find-file-dwim)
+        ("L"   projectile-find-file-dwim-other-window)
+        ("m"   projectile-compile-project)
+        ("o"   projectile-find-other-file)
+        ("O"   projectile-find-other-file-other-window)
+        ("p"   projectile-commander)
+        ("r"   projectile-recentf)
+        ("s"   projectile-multi-occur)
+        ("S"   projectile-replace)
+        ("t"   projectile-find-tag)
+        ("T"   projectile-regenerate-tags)
+        ("u"   projectile-find-test-file)
+        ("U"   projectile-test-project)
+        ("v"   projectile-display-buffer)
+        ("V"   projectile-ibuffer)
+        ("X"   fixmee-mode)
+        ("x"   fixmee-view-listing)))
+
+(global-set-key (kbd "H-s")
+(defhydra hydra-learn-sp (:hint nil)
+  "
+  _B_ backward-sexp            -----
+  _F_ forward-sexp               _s_ splice-sexp
+  _L_ backward-down-sexp         _df_ splice-sexp-killing-forward
+  _H_ backward-up-sexp           _db_ splice-sexp-killing-backward
+^^------                         _da_ splice-sexp-killing-around
+  _k_ down-sexp                -----
+  _j_ up-sexp                    _C-s_ select-next-thing-exchange
+-^^-----                         _C-p_ select-previous-thing
+  _n_ next-sexp                  _C-n_ select-next-thing
+  _p_ previous-sexp            -----
+  _a_ beginning-of-sexp          _C-f_ forward-symbol
+  _z_ end-of-sexp                _C-b_ backward-symbol
+--^^-                          -----
+  _t_ transpose-sexp             _c_ convolute-sexp
+-^^--                            _g_ absorb-sexp
+  _x_ delete-char                _q_ emit-sexp
+  _dw_ kill-word               -----
+  _dd_ kill-sexp                 _,b_ extract-before-sexp
+-^^--                            _,a_ extract-after-sexp
+  _S_ unwrap-sexp              -----
+-^^--                            _AP_ add-to-previous-sexp
+  _C-h_ forward-slurp-sexp       _AN_ add-to-next-sexp
+  _C-l_ forward-barf-sexp      -----
+  _C-S-h_ backward-slurp-sexp    _ join-sexp
+  _C-S-l_ backward-barf-sexp     _|_ split-sexp
+"
+  ;; TODO: Use () and [] - + * | <space>
+  ("B" sp-backward-sexp );; similiar to VIM b
+  ("F" sp-forward-sexp );; similar to VIM f
+  ;;
+  ("L" sp-backward-down-sexp )
+  ("H" sp-backward-up-sexp )
+  ;;
+  ("k" sp-down-sexp ) ; root - towards the root
+  ("j" sp-up-sexp )
+  ;;
+  ("n" sp-next-sexp )
+  ("p" sp-previous-sexp )
+  ;; a..z
+  ("a" sp-beginning-of-sexp )
+  ("z" sp-end-of-sexp )
+  ;;
+  ("t" sp-transpose-sexp )
+  ;;
+  ("x" sp-delete-char )
+  ("dw" sp-kill-word )
+  ;;("ds" sp-kill-symbol ) ;; Prefer kill-sexp
+  ("dd" sp-kill-sexp )
+  ;;("yy" sp-copy-sexp ) ;; Don't like it. Pref visual selection
+  ;;
+  ("S" sp-unwrap-sexp ) ;; Strip!
+  ;;("wh" sp-backward-unwrap-sexp ) ;; Too similar to above
+  ;;
+  ("C-h" sp-forward-slurp-sexp )
+  ("C-l" sp-forward-barf-sexp )
+  ("C-S-h" sp-backward-slurp-sexp )
+  ("C-S-l" sp-backward-barf-sexp )
+  ;;
+  ;;("C-[" (bind (sp-wrap-with-pair "[")) ) ;;FIXME
+  ;;("C-(" (bind (sp-wrap-with-pair "(")) )
+  ;;
+  ("s" sp-splice-sexp )
+  ("df" sp-splice-sexp-killing-forward )
+  ("db" sp-splice-sexp-killing-backward )
+  ("da" sp-splice-sexp-killing-around )
+  ;;
+  ("C-s" sp-select-next-thing-exchange )
+  ("C-p" sp-select-previous-thing )
+  ("C-n" sp-select-next-thing )
+  ;;
+  ("C-f" sp-forward-symbol )
+  ("C-b" sp-backward-symbol )
+  ;;
+  ;;("C-t" sp-prefix-tag-object)
+  ;;("H-p" sp-prefix-pair-object)
+  ("c" sp-convolute-sexp )
+  ("g" sp-absorb-sexp )
+  ("q" sp-emit-sexp )
+  ;;
+  (",b" sp-extract-before-sexp )
+  (",a" sp-extract-after-sexp )
+  ;;
+  ("AP" sp-add-to-previous-sexp );; Difference to slurp?
+  ("AN" sp-add-to-next-sexp )
+  ;;
+  ("_" sp-join-sexp ) ;;Good
+  ("|" sp-split-sexp ))))
+
 (provide 'my-hydras)
 ;;; my-hydras.el ends here
