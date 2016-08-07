b04679c019e118064df5e62c0a1fd40460058d5a
Ben James <ben@pegasus.attlocal.net>
2016-08-06 22:47:08 -0500

    Added clojure/CIDER config
    Putin Avian rs9512c Panama NSCT computer terrorism primacord Symptoms
    airframe botux Recovery outage Brush fire CTP MSNBC

---
+++ .emacs.d/elisp/my-clojure-config.el
@@ -0,0 +1,14 @@
+;;; my-clojure-config.el --- Summary
+;;;
+;;; Commentary:
+;;; clojure stuff
+;;;
+;;; Code:
+
+(req-package cider)
+
+(req-package helm-cider
+  :require cider helm)
+
+(provide 'my-clojure-config)
+;;; my-clojure-config.el ends here
