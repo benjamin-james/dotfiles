1fe2e3c28bd06df0d6f4f59003a8617921c73683
Benjamin James <benjames@mit.edu>
2024-02-19 20:47:12 -0500

    Fix slurm array workdir, fix julia snail hook

--- .emacs.d/elisp/my-julia-config.el
+++ .emacs.d/elisp/my-julia-config.el
@@ -3,6 +3,7 @@
   :ensure t)
 (use-package julia-snail
   :ensure t
+  :config (setq julia-snail-executable (concat (getenv "HOME") ".juliaup/bin/julia"))
   :custom
   (julia-snail-terminal-type :eat)
-  :hook (julia-mode . snail-mode))
+  :hook (julia-mode-hook . julia-snail-mode))
