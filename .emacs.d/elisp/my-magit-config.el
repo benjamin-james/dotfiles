7a90731905c14988cc90627abbebe8f4f8ce57e2
Benjamin James <benjames@mit.edu>
2024-03-13 20:35:06 -0400

    Readded spook to commit msg
    csystems Avalanche COSMOS Cocaine Saddam Hussein Hazardous UNSCOM NOC
    Freeh SSL Phreaking DES hate Los Zetas Blowfish

--- .emacs.d/elisp/my-magit-config.el
+++ .emacs.d/elisp/my-magit-config.el
@@ -1,6 +1,13 @@
 
+(defun magit-spook ()
+  (progn
+    (goto-char (point-min))
+    (insert "\n")
+    (spook)
+    (goto-char (point-min))))
 
 (use-package magit
+  :config (add-hook 'git-commit-setup-hook 'magit-spook)
   :bind (("C-x g" . magit-status)
          ("C-x C-g" . magit-status)))
 
