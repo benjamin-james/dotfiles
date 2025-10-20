8b02318dff9182e563d60962cce3568739a62690
Benjamin James <benjames@mit.edu>
2025-10-20 12:03:21 -0400

    more tmux stuff

--- .config/shell/features.d/ps1.sh
+++ .config/shell/features.d/ps1.sh
@@ -2,21 +2,25 @@
 [ -r "${_DOTBASE}/lib/helpers.sh" ] && . "${_DOTBASE}/lib/helpers.sh"
 _is_interactive || return 0
 
-if [ -n "$TMUX" ]; then
-  TMUX_TTY=$(tmux display-message -p '#{pane_tty}')
-  TMUX_PTS=${TMUX_TTY##*/}  
-  TMUX_HOST=$(hostname -s)
-  TMUX_STY="$(tmux display-message -p '#S').${TMUX_PTS}.${TMUX_HOST}"
+if _have tmux; then
+    tmux_sock="${TMUX%%,*}"
+    if [ -n "$TMUX" ] && [ -S "${tmux_sock}" ] && tmux -S "${tmux_sock}" list-sessions >/dev/null 2>&1; then
+	TMUX_TTY=$(tmux -S "${tmux_sock}" display-message -p '#{pane_tty}')
+	TMUX_PTS=${TMUX_TTY##*/}
+	TMUX_HOST=$(hostname -s)
+	TMUX_STY="$(tmux -S ${tmux_sock} display-message -p '#S').${TMUX_PTS}.${TMUX_HOST}"
+    else
+	unset TMUX
+    fi
 fi
-
 _session_label=$(hostname -s 2>/dev/null || hostname)
 if [ -n "$STY" ]; then
     _session_label="$STY"
-# elif [ -n "$TMUX_STY" ]; then
-#     _session_label="$TMUX_STY"
+elif [ -n "$TMUX_STY" ]; then
+    _session_label="$TMUX_STY"
 fi
 
-if command -v tput >/dev/null 2>&1 && [ "$(tput colors 2>/dev/null)" -ge 8 ]; then
+if _have tput && [ "$(tput colors 2>/dev/null)" -ge 8 ]; then
   # Using tput inline; \[...\] protects nonprinting chars for bash
   PS1="\[\e]0;\u@${_session_label}: \w\a\]"\
 "$(tput bold)$(tput setaf 2)\u@${_session_label}$(tput sgr0):"\
