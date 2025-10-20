6d8be64fb9911993e804c834894147c533508b66
Benjamin James <benjames@mit.edu>
2025-10-20 01:20:11 -0400

    krb5 linux only

--- .config/shell/features.d/openssh.sh
+++ .config/shell/features.d/openssh.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
 mkdir -p "$HOME/.ssh/cm_socket" "$HOME/.cache"
-export KRB5CCNAME="DIR:${HOME}/.cache/krb5cc"
--- .config/shell/os.d/linux.sh
+++ .config/shell/os.d/linux.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 [ -r "${_DOTBASE}/lib/helpers.sh" ] && . "${_DOTBASE}/lib/helpers.sh"
 _prepend_path "$HOME/.local/bin"
+mkdir -p "${HOME}/.cache"
+
+export KRB5CCNAME="DIR:${HOME}/.cache/krb5cc"
