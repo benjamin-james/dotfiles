04017879c8e904356ca5d0528a2510d37ec80c52
Benjamin James <benjames@mit.edu>
2026-05-19 13:43:04 -0400

    better CSAIL detection by checking $HOME

--- .config/shell/host.d/csail.sh
+++ .config/shell/host.d/csail.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-if hostname -d 2>&1 | grep -Fwq "csail.mit.edu"; then
-    export MY_HOST="csail"
-    export DATA_DIR="/data/scratch/benjames/"
-    export MY_CONDA_DIR="${DATA_DIR}/miniconda3/"
-    export UV_CACHE_DIR="${DATA_DIR}/uv-cache"
-    export XDG_CACHE_HOME="${DATA_DIR}/.cache"
+if [ "$HOME" = "/afs/csail.mit.edu/u/b/$USER" ]; then
+	export MY_HOST="csail"
+	export DATA_DIR="/data/scratch/$USER/"
+	export MY_CONDA_DIR="${DATA_DIR}/miniconda3/"
+	export UV_CACHE_DIR="${DATA_DIR}/uv-cache"
+	export XDG_CACHE_HOME="${DATA_DIR}/.cache"
 fi
