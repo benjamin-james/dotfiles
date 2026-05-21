3f0233190437c65d5cad54100f4e6e7cef4bb264
Benjamin James <benjames@mit.edu>
2026-05-21 16:15:11 -0400

    other variables extract from bashrc

--- .emacs.d/elisp/2-my-exec.el
+++ .emacs.d/elisp/2-my-exec.el
@@ -1,9 +1,14 @@
 
 (use-package exec-path-from-shell
-  :when (memq window-system '(mac ns x))
+  :when (or (memq window-system '(mac ns x))
+            (daemonp))
   :init
   (message "Loading exec path from shell")
+  (dolist (var '("GOPATH" "GPG_TTY" "SSH_AUTH_SOCK"
+                 "MY_PKGSRC" "MY_EPREFIX" "MACPORTS_PREFIX"))
+    (exec-path-from-shell-copy-env var))
   (exec-path-from-shell-initialize))
+
 
 (provide '2-my-exec)
 
