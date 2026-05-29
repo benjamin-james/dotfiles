ad332be0d467c28737414f374f12f0302d2c4edb
Benjamin James <benjames@mit.edu>
2026-05-29 11:23:48 -0400

    rustup shouldn't be required

--- .emacs.d/elisp/my-rust-config.el
+++ .emacs.d/elisp/my-rust-config.el
@@ -2,8 +2,8 @@
 (use-package rustic
   :init
   (add-to-list 'exec-path (expand-file-name "~/.cargo/bin/"))
-  :custom
-  (rustic-analyzer-command '("rustup" "run" "stable" "rust-analyzer"))
+  ;; :custom
+  ;; (rustic-analyzer-command '("rustup" "run" "stable" "rust-analyzer"))
   :config
   (setq rustic-format-on-save nil) ; apheleia handles instead
   (setq rustic-lsp-client 'eglot))
