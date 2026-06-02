833c2d7abf01adbebe8e655464c0a73510948151
Benjamin James <benjames@mit.edu>
2026-06-02 16:09:02 -0400

    use gtk-launch for gv/oku
    Sleet Incident global LRTS Skytel $400 million in gold bullion CFC
    Department of Homeland Security JAVA USCOI IW Tijuana Critical
    infrastructure Audiotel Smuggling

--- .config/shell/rc.d/linux.sh
+++ .config/shell/rc.d/linux.sh
@@ -2,5 +2,17 @@
 _is_interactive || return 0
 
 gv() {
-    nohup gwenview "$@" >/dev/null 2>/dev/null &
+	if [ "$#" -eq 0 ]; then
+		exec gwenview --help
+	else
+		gtk-launch gwenview "$@" >/dev/null 2>/dev/null
+	fi
+}
+
+oku() {
+	if [ "$#" -eq 0 ]; then
+		exec okular --help
+	else
+		gtk-launch okular "$@" >/dev/null 2>/dev/null
+	fi
 }
