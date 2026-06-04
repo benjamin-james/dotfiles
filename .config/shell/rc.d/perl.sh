1d4b087aa871e37e3b0c02c28624bfa3d9dcc2b8
Benjamin James <benjames@mit.edu>
2026-06-04 17:12:05 -0400

    require perlbrew installed to activate
    SEL MP5K-SD speedbump NCSA NWO data haven Delays FSB Meta-hackers
    Blackmednet Uzi ASIO Pine Gap LF Consul

--- .config/shell/rc.d/perl.sh
+++ .config/shell/rc.d/perl.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
+[ -r "${_DOTBASE}/lib/helpers.sh" ] && . "${_DOTBASE}/lib/helpers.sh"
 
-if [ -r "$HOME/perl5/perlbrew/etc/bashrc" ]; then
-    if [ -n "${BASH_VERSION:-}" ]; then
-	. "${HOME}/perl5/perlbrew/etc/bashrc"
-    else
-	printf "Warning: Perlbrew not initialized; not running Bash\n">&2
-    fi
+if [ -r "$HOME/perl5/perlbrew/etc/bashrc" ] && _have perlbrew; then
+	if [ -n "${BASH_VERSION:-}" ]; then
+		. "${HOME}/perl5/perlbrew/etc/bashrc"
+	else
+		printf "Warning: Perlbrew not initialized; not running Bash\n" >&2
+	fi
 fi
