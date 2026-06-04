51f2334f0479fcfabd7480790a8cf4657aa27608
Benjamin James <benjames@mit.edu>
2026-06-04 17:10:30 -0400

    don't recursively source dotbase especially before env.d
    Homeland security event security Agro UOP Mexican army Spetznaz
    dictionary Macintosh Firewalls Cap-Stun U.S. Citizenship and
    Immigration Services government Suicide attack Human to Animal Grid
    MI5

--- .config/shell/login.sh
+++ .config/shell/login.sh
@@ -1,19 +1,18 @@
 #!/bin/sh
 if [ "${_DOT_LOGIN_SOURCED:-0}" -eq 1 ]; then
-  return
+	return
 fi
 _DOT_LOGIN_SOURCED=1
 _DOTBASE=${_DOTBASE:-"$HOME/.config/shell"}
 
 [ -r "${_DOTBASE}/lib/helpers.sh" ] && . "${_DOTBASE}/lib/helpers.sh"
 
-_sourcedir "${_DOTBASE}"
 _sourcedir "${_DOTBASE}/env.d"
 
 OS=$(uname -s 2>/dev/null || echo unknown)
 case "$OS" in
-    Darwin) [ -r "${_DOTBASE}/os.d/darwin.sh" ] &&  . "${_DOTBASE}/os.d/darwin.sh" ;;
-    Linux) [ -r "${_DOTBASE}/os.d/linux.sh" ] &&  . "${_DOTBASE}/os.d/linux.sh" ;;
+Darwin) [ -r "${_DOTBASE}/os.d/darwin.sh" ] && . "${_DOTBASE}/os.d/darwin.sh" ;;
+Linux) [ -r "${_DOTBASE}/os.d/linux.sh" ] && . "${_DOTBASE}/os.d/linux.sh" ;;
 esac
 
 _sourcedir "${_DOTBASE}/host.d"
