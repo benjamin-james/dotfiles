9b7e3e202887f61dc982c856a070c68358815bd9
Benjamin James <benjames@mit.edu>
2026-06-06 10:55:19 -0400

    KWin script debugging
    E.T. CRA Norvo Virus Information Security Water borne M-14 CBNRC MD4
    Albright Radicals Euskadi ta Askatasuna Al-Qaeda Al Qaeda in the
    Islamic Maghreb Fundamentalism CSE

--- .config/shell/os.d/linux.sh
+++ .config/shell/os.d/linux.sh
@@ -2,3 +2,9 @@
 [ -r "${_DOTBASE}/lib/helpers.sh" ] && . "${_DOTBASE}/lib/helpers.sh"
 _prepend_path "$HOME/.local/bin"
 mkdir -p "${HOME}/.cache"
+
+if [ -n "${QT_LOGGING_RULES:-}" ]; then
+	## KWin script debugging
+	QT_LOGGING_RULES="kwin_*.debug=true"
+	export QT_LOGGING_RULES
+fi
