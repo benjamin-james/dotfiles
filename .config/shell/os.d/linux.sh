6aca3fd7c1ac61dff236e570985fa6d49c3142f8
Benjamin James <benjames@mit.edu>
2026-06-06 17:05:21 -0400

    wrong condition
    Dock DMAT Macintosh Firewalls MI5 TSCI HIC Talent Minox MSNBC Mafia
    Leuken-Baden Dirty bomb sigvoice argus NBIC

--- .config/shell/os.d/linux.sh
+++ .config/shell/os.d/linux.sh
@@ -3,7 +3,7 @@
 _prepend_path "$HOME/.local/bin"
 mkdir -p "${HOME}/.cache"
 
-if [ -n "${QT_LOGGING_RULES:-}" ]; then
+if [ ! -n "${QT_LOGGING_RULES:-}" ]; then
 	## KWin script debugging
 	QT_LOGGING_RULES="kwin_*.debug=true"
 	export QT_LOGGING_RULES
