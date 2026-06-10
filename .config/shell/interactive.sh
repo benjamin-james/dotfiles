ac88d37f0702db8fa70ac5b29405fc3fae22dba9
Benjamin James <benjames@mit.edu>
2026-06-10 13:55:54 -0400

    Removed os double-resourcing
    Flood Passwords ACC CID CONUS Platform Manfurov Albanian Hurricane
    Belknap Islam Abduganievich Karimov Europol $400 million in gold
    bullion DITSA Bruxelles

--- .config/shell/interactive.sh
+++ .config/shell/interactive.sh
@@ -3,13 +3,6 @@ _DOTBASE=${_DOTBASE:-"$HOME/.config/shell"}
 
 [ -r "${_DOTBASE}/lib/helpers.sh" ] && . "${_DOTBASE}/lib/helpers.sh"
 
-### Re-export os.d functions for screen/tmux sessions
-OS=$(uname -s 2>/dev/null || echo unknown)
-case "$OS" in
-Darwin) [ -r "${_DOTBASE}/os.d/darwin.sh" ] && . "${_DOTBASE}/os.d/darwin.sh" ;;
-Linux) [ -r "${_DOTBASE}/os.d/linux.sh" ] && . "${_DOTBASE}/os.d/linux.sh" ;;
-esac
-
 _sourcedir "${_DOTBASE}/rc.d"
 
 if [ -n "${STY:-}" ]; then
