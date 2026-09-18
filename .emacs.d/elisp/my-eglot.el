cffe995999e34dc707311191e95cb07c44d66e2e
Benjamin James <benjames@mit.edu>
2026-09-18 13:11:34 -0400

    typo in eglot config
    SGI Pretoria lynch DEA SGDN Secure Border Initiative Perl-RSA ISS hate
    IDF botux advisors Drug Enforcement Agency pink noise Denial of
    service

--- .emacs.d/elisp/my-eglot.el
+++ .emacs.d/elisp/my-eglot.el
@@ -22,7 +22,7 @@
   (add-hook 'prog-mode-hook #'eglot-ensure)
   :config
   (setq eglot-stay-out-of '(company))
-  (add-to-list 'eglot-ignored-server-capabilites :documentOnTypeFormattingProvider) ;; use other formatting tools instead
+  (add-to-list 'eglot-ignored-server-capabilities :documentOnTypeFormattingProvider) ;; use other formatting tools instead
   (add-to-list 'eglot-server-programs '((python-mode python-ts-mode) . ("pylsp")))
   (add-to-list 'eglot-server-programs '((cperl-mode perl-mode perl-ts-mode) . ("perlbrew" "exec" "pls")))
   (setq-default eglot-workspace-configuration
